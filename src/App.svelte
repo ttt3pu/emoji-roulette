bb5e7184475d067c1998727ca0e51aed9f439003
attt <ttt3pu@gmail.com>
2022-03-18 00:21:58 +0900

    ✨feat: components/atoms/emoji

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import Emoji from './components/atoms/Emoji.svelte';
   export let name: string;
 </script>
 
 <main>
   <h1>Hello {name}!</h1>
   <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Emoji emoji="😀" />
+  <Emoji emoji="🐶" />
+  <Emoji emoji="😺" />
 </main>
 
 <style>
---
+++ src/components/atoms/Emoji.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import twemoji from 'twemoji';
+  export let emoji: string;
+  const emojiCode = twemoji.convert.toCodePoint(emoji);
+  export const src = `https://twemoji.maxcdn.com/v/latest/svg/${emojiCode}.svg`;
+</script>
+
+<main>
+  <img src={src} alt={emoji}>
+</main>
+
+<style>
+img {
+  width: 30px;
+  height: 30px;
+}
+</style>
